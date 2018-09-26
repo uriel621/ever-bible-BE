d592a3789098c46a348655b673d7eddde95eb828
Ever <uriel621@live.com>
2018-09-25 22:59:24 -0700

    bible api call setup

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
+import requests
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+def all_bible():
+    url = "https://api.scripture.api.bible/v1/bibles"
+    headers = {'Content-Type':'application/x-www-form-urlencoded', 'api-key': '81f316c5f31960d155555818b8d0a59c'}
+    response = requests.request("GET", url, headers=headers)
+    return response.json()
+
+@app.route('/', methods=['GET'])
+def get_tasks():
+    return jsonify(all_bible())
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
